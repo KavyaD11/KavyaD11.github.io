d813d9cbcbf25cbe90d2aeabedd154c73a07a76b
KAVYA <kavyadhawale@gmail.com>
2024-06-14 01:52:17 +0530

    EVARA

--- main.js
+++ main.js
@@ -1,32 +1,21 @@
-function post() {
-  var sellerName = document.getElementById("sellername").value;
-  var sellerNum = document.getElementById("sellernum").value;
-  var productTitle = document.getElementById("productName").value;
-  var price = document.getElementById("price").value;
-  var Location = document.getElementById("Location").value;
-  var productDetail = document.getElementById("productDetail").value;
-  var Image1 = document.getElementById("img1").value;
-  var Image2 = document.getElementById("img2").value;
-  var Image3 = document.getElementById("img3").value;
-  var obj = {
-    sellername: sellerName,
-    sellernum: sellerNum,
-    product: productTitle,
-    price: price,
-    image1: Image1,
-    image2: Image2,
-    image3: Image3,
-    description: productDetail,
-    postTime: Location,
-  };
+        function postAd() {
+            const sellerName = document.getElementById('sellername').value;
+            const sellerNum = document.getElementById('sellernum').value;
+            const productName = document.getElementById('productName').value;
+            const price = document.getElementById('price').value;
+            const location = document.getElementById('Location').value;
+            const productDetail = document.getElementById('productDetail').value;
+            const img1 = document.getElementById('img1').value;
+            const img2 = document.getElementById('img2').value;
+            const img3 = document.getElementById('img3').value;
 
-  var getData = localStorage.getItem("DATA");
-  var Arrangedata = JSON.parse(getData);
-  Arrangedata.push(obj);
-  localStorage.setItem("DATA", JSON.stringify(Arrangedata));
-  // console.log(Arrangedata);
-  location.href = "user.html";
-  // match = false;
-  // sessionStorage.setItem("SetData", JSON.stringify(data));
-  // console.log(setdata);
-}
+            console.log('Seller Name:', sellerName);
+            console.log('Seller Number:', sellerNum);
+            console.log('Product Name:', productName);
+            console.log('Price:', price);
+            console.log('Location:', location);
+            console.log('Product Details:', productDetail);
+            console.log('Image 1 URL:', img1);
+            console.log('Image 2 URL:', img2);
+            console.log('Image 3 URL:', img3);
+
